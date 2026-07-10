f7dde58a90e3aaed8504e1dca81503cd0ad6a309
Marek Kowalczyk <dsw60330@student.dsw.edu.pl>
2026-07-11 01:14:42 +0200

    Fix flake8 errors

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-key-tylko-lokalnie")
 
