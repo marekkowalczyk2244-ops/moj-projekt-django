59be7cf5530d11a1ecb9a93296d8f25e4caa576d
Marek Kowalczyk <dsw60330@student.dsw.edu.pl>
2026-07-11 01:47:20 +0200

    Fix flake8 style issues (W391, E305, E402, W292)

--- config/settings.py
+++ config/settings.py
@@ -127,4 +127,3 @@ STATIC_URL = "static/"
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
 CELERY_TASK_ALWAYS_EAGER = False
-
